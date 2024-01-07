2e954d793a33020c33d8701bad4b1758efbb6a42
krisztikodok <135458893+krisztikodok@users.noreply.github.com>
2024-01-07 17:22:45 +0000

    w/ multiple custom ex.

--- main.py
+++ main.py
@@ -28,3 +28,31 @@ except ValueError:
   print("Invalid input. Please enter an integer.")
 
 #multiple user-defined exceptions
+
+class type1(Exception):
+  def __init__(self,val,expected_type):
+    self.val=val
+    self.expected_type=expected_type
+    super().__init__(f"Value {val} is not type {expected_type}.")
+
+class val2(Exception):
+  def __init__(self,msg):
+    self.msg=msg
+    super().__init__(f"The following message has reached you: {msg}") #this msg comes first,then the called exc. msg
+
+try:
+  my_input=input("Enter an integer:")
+  if not my_input.isdigit():
+    raise val2("Invalid input,please enter an integer")
+
+  my_input=int(my_input)
+  if not isinstance(my_input,int):  #returns true if the specified value is the specified type
+    raise type1(my_input,int)
+  else:
+    print(f"Your value is: {my_input}")
+except type1 as t:
+  print(f"Caught error: {t}")
+except val2 as v:
+  print(f"Caught error: {v}")
+except ValueError:
+  print("Invalid input. Please enter an integer.")
