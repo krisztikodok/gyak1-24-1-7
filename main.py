bbd020a79e5a4a5a91fb21f675ba3a2611f031f0
krisztikodok <135458893+krisztikodok@users.noreply.github.com>
2024-01-07 17:08:31 +0000

    added w/ spec. conditions

--- main.py
+++ main.py
@@ -12,4 +12,19 @@ except custom1 as c:
 
 #user-defined exc. with specific conditions
 class valTooSmall(Exception):
-  
+  def __init__(self,val):
+    self.val=val
+    super().__init__(f"value {val}: is too small, it should be greater than 10")
+
+try:
+  user_input=int(input("Enter a nr greater than 10:"))
+  if user_input<=10:
+    raise valTooSmall(user_input) #if smaller than 10,it calls valTooSmall exception
+  else:
+    print(f"The number is {user_input}")
+except valTooSmall as v:
+  print(f"Error at {v}")
+except ValueError:
+  print("Invalid input. Please enter an integer.")
+
+#multiple user-defined exceptions
